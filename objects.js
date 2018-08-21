079827e70a55ea78e1551c987c5e0968443b7535
Abbey Mondshein <abbey.sync@gmail.com>
2018-08-21 00:17:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,8 @@ var playlist = { artist: "song title"};
 //Create a function updatePlaylist that accepts three parameters: the playlist (an object), an artist name (a string), and a song title. The body of the function should add the song and artist as a key-value pair to the playlist object. The function should return the whole playlist.
 
 function updatePlaylist(playlist,artistName,songTitle){
-  
+  playlist.artistName
+  return playlist
 }
 
 //Create a function removeFromPlaylist that accepts two arguments (the playlist object and the artist name). The body of the function should delete the key-value pair from the playlist and return the updated playlist.
